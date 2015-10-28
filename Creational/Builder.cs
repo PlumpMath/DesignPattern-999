dca9176b56c88427fad1bd5be34c4fc3f2e8efc5
Petar M <pminev@hotmail.com>
2015-10-28 22:17:29 +0200

    add new Creation DP

---
+++ Creational/Builder.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace designPatterns.Creational
+{
+    public class Builder
+    {
+        public void Demonstrate()
+        {
+            var CarModels = new CarModel();
+            CarModels.SetModel("Bmw");
+            CarModels.SetWheels("4");
+
+            var newObject = CarModels.Buid();
+
+            Console.WriteLine($"the new car model {newObject.Model}");
+
+        }
+
+
+        public class CarModel
+        {
+            public string Model { get; set; }
+
+            public string Wheels { get; set; }
+
+
+            public void SetModel(string model)
+            {
+                Model = model;
+            }
+
+            public void SetWheels(string wheels)
+            {
+                Wheels = wheels;
+            }
+
+            public CarModel Buid()
+            {
+                return this;
+            }
+        }
+    }
+}
---
+++ Creational/Factory.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace designPatterns.Creational
+{
+  public class Factory
+    {
+      public void Demonstrate()
+      {
+          ICar NewCar;
+
+          Console.WriteLine("we are setting New Car as Ford");
+            NewCar = new Ford();
+          Console.WriteLine(NewCar.GetCarModel());
+          
+
+          Console.WriteLine("we are changin it to bmw");
+            NewCar = new BMW();
+            Console.WriteLine(NewCar.GetCarModel());
+        }
+    }
+
+
+    public abstract class AbstractFactory
+    {
+        public static ICar CreateCar(int carID)
+        {
+            switch (carID)
+            {
+                case 1:
+                {
+                    return new Ford();
+                }
+                case 2:
+                {
+                    return new BMW();
+                }
+                default:
+                {
+                    return null;
+                }   
+            }
+        }
+    }
+
+    public interface ICar
+    {
+        string GetCarModel();
+    }
+
+
+    public class Ford : ICar
+    {
+        public string GetCarModel()
+        {
+            return "Ford carr this is new ford";
+        }
+
+    }
+
+
+    public class BMW:ICar
+    {
+        public string GetCarModel()
+        {
+            return "BMW Car is comming";
+        }
+    }
+}
