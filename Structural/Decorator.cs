84c9aaf366d8f0f1b536e359140561a9f27689bc
Petar M <pminev@hotmail.com>
2015-11-05 00:18:50 +0200

    adsa

---
+++ Structural/Decorator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace designPatterns.Structural
+{
+  public class Decorator
+    {
+    }
+
+
+    public abstract class BakeryComponent
+    {
+        public abstract string GetName();
+        public abstract double GetPrice();
+
+    }
+
+    public class  CackeBase:BakeryComponent
+    {
+        private string _cackeName = "Base Cakce";
+        private double _cackePrice = 12;
+
+        public override string GetName()
+        {
+            return _cackeName;
+        }
+
+        public override double GetPrice()
+        {
+            return _cackePrice;
+        }
+    }
+
+
+    public abstract class Decorator : BakeryComponent
+    {
+
+
+        
+    }
+}
