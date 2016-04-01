835b0e6d9f3d766966f249e606ac7383015bae66
pminev <pminev@hotmail.com>
2016-04-01 16:03:53 +0300

    Facade

--- Structural/Facade.cs
+++ Structural/Facade.cs
@@ -6,38 +6,90 @@ using System.Threading.Tasks;
 
 namespace designPatterns.Structural
 {
-  public class  Facade
+  public class  FacadeDP
     {
-      public void StartJogging()
-      {
-          Console.WriteLine("Start Jogging");
-        
-            var wifi = new Wifi("running wifi");
 
-          Console.WriteLine("started wifi for jogging");
+        public void Demonstrate()
+        {
+            var facadetmp = new Facade();
 
-          RunningTIme.StartRunning();
-          Console.WriteLine("start runnning...");
-      }
+            Console.WriteLine(facadetmp.GetTempretureInformation("111"));
+        }
+     
+
+      
+
     }
 
 
-    public class Wifi
-    {
-        public string Name { get; set; }
 
-        public Wifi(string Name)
+
+
+    public class Facade
+    {
+        private readonly TempretureService tempService;
+
+        public Facade() 
         {
-            this.Name= Name;
+            tempService = new TempretureService();
+
+        }
+
+
+        public string GetTempretureInformation(string zipcode)
+        {
+            var city = tempService.GetCityByZip(zipcode);
+            var state = tempService.GetStateByCity(city);
+            var temp = tempService.GetTempretureByCity(city);
+
+            return string.Format("curren ttempreture is {0} in {1} , {2}", temp, city, state);
+        }
+
+    }
+
+
+    #region Anti Design Pattern
+
+    /// <summary>
+    /// anti patern shows that if w eare calling the service without facade we have the following porblem, we need to know the complexity of the api that we need instead of using it directlyu 
+    /// </summary>
+    public class FacadeAnti
+    {
+
+        public string GetTempreture(string zipCode)
+        {
+            var tempService = new TempretureService();
+
+            var city = tempService.GetCityByZip(zipCode);
+            var state = tempService.GetStateByCity(city);
+            var temp = tempService.GetTempretureByCity(city);
+            
+            return string.Format("curren ttempreture is {0} in {1} , {2}",temp,city,state);
         }
     }
 
-    public static class RunningTIme
+    #endregion 
+
+
+    /// <summary>
+    /// Service that will be called from the DP and Anti DP 
+    /// </summary>
+    public class TempretureService
     {
-        public static string StartRunning()
+        public string GetCityByZip(string ZipCode)
         {
-            return "Running";
+            return "Sofia";
         }
 
+        public string GetStateByCity(string City)
+        {
+            return "Sofia-grad";
+        }
+
+        public string GetTempretureByCity(string City)
+        {
+            return string.Format("{0} F1",20);
+        }
     }
+
 }
